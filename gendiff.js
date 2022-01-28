c7fcc5fc828229387302cb77dd5e5dac773c3f77
idavidenko.ext@diamos.com <idavidenko.ext@diamos.com>
2022-01-28 14:37:35 +0300

    second

--- gendiff.js
+++ gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import program  from 'commander';
 
+
+
 program
     .description('Compares two configuration files and shows a difference.') // command description
     .version('output the version number')
-    .helpOption('-h, --HELP', 'output usage information')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+
     // function to execute when command is uses
     .action(function () {
 
