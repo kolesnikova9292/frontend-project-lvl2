4691d3590df2193949deb3f9aabf45f6555c23e9
id <iuliia.davidenko@diamos.com>
2023-11-16 18:33:27 +0100

    рекурсивное сравнение

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -16,7 +16,16 @@ test('parsing', () => {
     const json1 = JSON.parse(data);
     const json2 = JSON.parse(data1);
 
-    const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
+   // const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
+
+    const result = '{\n' +
+        ' - follow: false\n' +
+        '   host: hexlet.io\n' +
+        ' - proxy: 123.234.53.22\n' +
+        ' + timeout: 50\n' +
+        ' - timeout: 20\n' +
+        ' + verbose: true\n' +
+        '}';
 
 
     expect(parsing(json1, json2)).toStrictEqual(result);
@@ -30,7 +39,76 @@ test('parsing2', () => {
     const json1 = yaml.load(data);
     const json2 = yaml.load(data1);
 
-    const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
+    //const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
+    const result = '{\n' +
+        ' - follow: false\n' +
+        '   host: hexlet.io\n' +
+        ' - proxy: 123.234.53.22\n' +
+        ' + timeout: 50\n' +
+        ' - timeout: 20\n' +
+        ' + verbose: true\n' +
+        '}'
+
+
+    expect(parsing(json1, json2)).toStrictEqual(result);
+});
+
+
+test('parsing', () => {
+
+
+    const data = fs.readFileSync('__tests__/__fixtures__/json2/file1.json', 'utf8');
+    const data1 = fs.readFileSync('__tests__/__fixtures__/json2/file2.json', 'utf8');
+    const json1 = JSON.parse(data);
+    const json2 = JSON.parse(data1);
+
+    // const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
+
+    const result = '{\n' +
+        '   common: {\n' +
+        '    - follow: {\n' +
+        '      }\n' +
+        '      setting1: Value 1\n' +
+        '    - setting2: 200\n' +
+        '    - setting3: true\n' +
+        '    + setting4: blah blah\n' +
+        '    + setting5: {\n' +
+        '       key5: value5\n' +
+        '      }\n' +
+        '      setting6: {\n' +
+        '         doge: {\n' +
+        '          + wow: so much\n' +
+        '         }\n' +
+        '         key: value\n' +
+        '       + ops: vops\n' +
+        '      }\n' +
+        '   }\n' +
+        '   group1: {\n' +
+        '    + baz: bas\n' +
+        '    - baz: bars\n' +
+        '      foo: bar\n' +
+        '      nest: {\n' +
+        '       + 0: s\n' +
+        '       + 1: t\n' +
+        '       + 2: r\n' +
+        '       - key: value\n' +
+        '      }\n' +
+        '   }\n' +
+        ' - group2: {\n' +
+        '    abc: 12345\n' +
+        '    deep: {\n' +
+        '     id: 45\n' +
+        '    }\n' +
+        '   }\n' +
+        ' + group3: {\n' +
+        '    deep: {\n' +
+        '     id: {\n' +
+        '      number: 45\n' +
+        '     }\n' +
+        '    }\n' +
+        '    fee: 100500\n' +
+        '   }\n' +
+        '}';
 
 
     expect(parsing(json1, json2)).toStrictEqual(result);
--- src/parsing.js
+++ src/parsing.js
@@ -1,82 +1,79 @@
 import lodash from "lodash";
 
-const parsing = (obj, obj2, replacer = ' ', spacesCount = 1,  result = '{\n', step = 0) => {
 
-    if (typeof obj === 'string' || typeof obj === 'boolean' || typeof obj === 'number') {
-        return obj.toString()
-    }
+const parsing = (json1, json2, replacer = ' ', spacesCount = 1,  result = '{\n', step = 1) => {
 
-    if (obj === null) {
-        return obj
-    }
-
-    let currentStep = step++;
-
-    for (let variable in obj) {
-        result = result + replacer.repeat(step * spacesCount) + variable + ': ';
-
-        if(typeof obj[variable] === 'object' && obj[variable] !== null) {
-            result = result + '{\n';
-            currentStep = currentStep + 1;
-        }
-
-        result = result + parsing(obj[variable], obj2[variable], replacer, spacesCount,  '' , currentStep) + '\n';
-    }
-
-    return result + replacer.repeat((step - 1)* spacesCount) + '}'
-}
-
-const parsing1 = (json1, json2) => {
+    const arrayWithInsertedProps = []
 
     const keysWithObjRef = Object.keys(json1).map((x) => {
         const result = { obj: 'json1', key: x };
         return result;
     });
 
-    console.log(keysWithObjRef)
-
     const keysWithObj2Ref = Object.keys(json2).map((x) => {
         const result = { obj: 'json2', key: x };
         return result;
     });
 
-    console.log(keysWithObj2Ref)
-
     const allKeys = lodash.sortBy([...keysWithObjRef, ...keysWithObj2Ref], (a) => a.key);
-
-    console.log(allKeys)
-
-    const obj = {};
+    //const allKeys = [...keysWithObjRef, ...keysWithObj2Ref];
 
     allKeys.forEach((x) => {
-        let currentSign = ' ';
-        if (Object.prototype.hasOwnProperty.call(json1, x.key)
-            && !Object.prototype.hasOwnProperty.call(json2, x.key)) {
-            currentSign = '-';
+
+        if(stringifyLittle(json1[x.key]) === stringifyLittle(json2[x.key]) && arrayWithInsertedProps.indexOf(x.key) === -1) {
+            arrayWithInsertedProps.push(x.key);
+            result = result + replacer.repeat(step * spacesCount) + '  ' + x.key + ': ' + stringifyLittle(json1[x.key], replacer, spacesCount, step) + '\n';
         }
-        if (!Object.prototype.hasOwnProperty.call(json1, x.key)
-            && Object.prototype.hasOwnProperty.call(json2, x.key)) {
-            currentSign = '+';
+
+        if(!json2[x.key] && arrayWithInsertedProps.indexOf(x.key) == -1) {
+            arrayWithInsertedProps.push(x.key);
+            result = result + replacer.repeat((step) * spacesCount) + '- ' + x.key + ': ' + stringifyLittle(json1[x.key], replacer, spacesCount, step+3) + '\n';
         }
-        if (Object.prototype.hasOwnProperty.call(json1, x.key)
-            && Object.prototype.hasOwnProperty.call(json2, x.key)
-            && json1[x.key] !== json2[x.key]) {
-            if (x.obj === 'json1') {
-                currentSign = '-';
+
+        if(!json1[x.key] && arrayWithInsertedProps.indexOf(x.key) == -1) {
+            arrayWithInsertedProps.push(x.key);
+            result = result + replacer.repeat(step * spacesCount) + '+ ' + x.key + ': ' + stringifyLittle(json2[x.key], replacer, spacesCount, step+3) + '\n';
+        }
+
+        if(stringifyLittle(json1[x.key]) !== stringifyLittle(json2[x.key]) && json1[x.key] && json2[x.key]  && arrayWithInsertedProps.indexOf(x.key) === -1) {
+            arrayWithInsertedProps.push(x.key);
+            if (typeof json1[x.key] === 'object' && json1[x.key] !== null) {
+                result = result + replacer.repeat(step * spacesCount) + '  ' + x.key + ': ';
+                result = result + parsing(json1[x.key], json2[x.key], replacer, spacesCount, '{\n', step+3) + '\n';
             } else {
-                currentSign = '+';
+                result = result + replacer.repeat((step) * spacesCount) + '+ ' + x.key + ': ';
+                result = result + stringifyLittle(json1[x.key], replacer, spacesCount, step+1) + '\n';
+
+                result = result + replacer.repeat((step) * spacesCount) + '- ' + x.key + ': ';
+                result = result + stringifyLittle(json2[x.key], replacer, spacesCount, step+1) + '\n';
             }
-        }
-        const field = `${currentSign} ${x.key}`;
-        if (x.obj === 'json1') {
-            obj[field] = json1[x.key];
-        } else {
-            obj[field] = json2[x.key];
         }
     });
 
-    return obj;
-
+    return result + replacer.repeat((step - 1) * spacesCount) + '}'
 };
+
+const stringifyLittle = (obj,
+                         replacer = ' ',
+                         spacesCount = 1,
+                         step = 1,
+                         firstSign = '',
+                         result = firstSign + '{\n'
+                         //result = firstSign + replacer.repeat((step - 1) * spacesCount) + '{\n'
+) => {
+    if (typeof obj === 'string' || typeof obj === 'boolean' || typeof obj === 'number') return obj.toString();
+
+    if (obj === null) return obj;
+
+    for (let variable in obj) {
+        result = result + replacer.repeat(step * spacesCount) + variable + ': ';
+        if(typeof obj[variable] === 'object' && obj[variable] !== null) {
+            // result = result + '{\n';
+            // step = step + 8;
+        }
+        result = result + stringifyLittle(obj[variable], replacer, spacesCount, step+1) + '\n';
+    }
+    return result + replacer.repeat((step - 1) * spacesCount) + '}'
+}
 
 export default parsing;
