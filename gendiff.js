0bdac27f87a6e7575859050760a519e20cab1b65
id <iuliia.davidenko@diamos.com>
2023-12-12 10:01:03 +0100

    default import

--- gendiff.js
+++ gendiff.js
@@ -11,74 +11,82 @@ import * as yaml from 'js-yaml';
 //const path = require('node:path');
 
 program
-  .description('Compares two configuration files and shows a difference.') // command description
-  .version('output the version number')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+    .description('Compares two configuration files and shows a difference.') // command description
+    .version('output the version number')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
 
-  // function to execute when command is uses
-  .action(() => {
+    // function to execute when command is uses
+    .action(() => {
 
-  });
-
-export default function main() {
-    program
-        .description('Compares two configuration files and shows a difference.')
-        .version('0.1.0')
-        .usage('[options] <file> <file>')
-        .arguments('<file> <file>')
-        .action((file, file2) => {
-            try {
-
-                //const path = require('path');
-
-                const extension1 = path.extname(file);
-                const extension2 = path.extname(file2);
+    });
 
 
-                if (extension1 === '.json' && extension2 === '.json') {
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.1.0')
+    .usage('[options] <file> <file>')
+    .arguments('<file> <file>')
+    .action((file, file2) => {
+        try {
 
-                    const data = fs.readFileSync(file, 'utf8');
-                    const data1 = fs.readFileSync(file2, 'utf8');
-                    const json1 = JSON.parse(data);
-                    const json2 = JSON.parse(data1);
+            main(file, file2);
 
-                    const resultObject = parsing(json1, json2);
-
-                    console.log(resultObject)
-
-                    //console.log('{');
-                    //Object.entries(resultObject).forEach((e) => console.log(`    ${e[0]}: ${e[1]}`));
-                    //console.log('}');
-
-                }
+            //const path = require('path');
 
 
-                if (extension1 === '.yaml' && extension2 === '.yaml') {
-
-                    const data = fs.readFileSync(file, 'utf8');
-                    const data1 = fs.readFileSync(file2, 'utf8');
-
-                    const doc = yaml.load(data);
-                    const doc2 = yaml.load(data1);
-                    //console.log(doc);
 
 
-                    //const json1 = JSON.parse(doc);
-                    //const json2 = JSON.parse(doc2);
-                    const resultObject = parsing(doc, doc2);
-                    console.log('{');
-                    Object.entries(resultObject).forEach((e) => console.log(`    ${e[0]}: ${e[1]}`));
-                    console.log('}');
-
-                }
-
-
-            } catch (err) {
-                console.error(err);
-            }
-        });
-}
+        } catch (err) {
+            console.error(err);
+        }
+    });
 
 //использую для тестирования
 //program.parse(process.argv);
+
+export default function main(file1, file2) {
+
+    const extension1 = path.extname(file1);
+    const extension2 = path.extname(file2);
+
+
+    if (extension1 === '.json' && extension2 === '.json') {
+
+        const data = fs.readFileSync(file, 'utf8');
+        const data1 = fs.readFileSync(file2, 'utf8');
+        const json1 = JSON.parse(data);
+        const json2 = JSON.parse(data1);
+
+        const resultObject = parsing(json1, json2);
+
+        console.log(resultObject)
+
+        //console.log('{');
+        //Object.entries(resultObject).forEach((e) => console.log(`    ${e[0]}: ${e[1]}`));
+        //console.log('}');
+
+    }
+
+
+    if (extension1 === '.yaml' && extension2 === '.yaml') {
+
+        const data = fs.readFileSync(file, 'utf8');
+        const data1 = fs.readFileSync(file2, 'utf8');
+
+        const doc = yaml.load(data);
+        const doc2 = yaml.load(data1);
+        //console.log(doc);
+
+
+        //const json1 = JSON.parse(doc);
+        //const json2 = JSON.parse(doc2);
+        const resultObject = parsing(doc, doc2);
+        console.log('{');
+        Object.entries(resultObject).forEach((e) => console.log(`    ${e[0]}: ${e[1]}`));
+        console.log('}');
+
+    }
+
+
+}
