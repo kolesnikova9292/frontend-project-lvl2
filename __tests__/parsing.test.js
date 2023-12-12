6afec3b89b2d5e8e63ae0e5ce331219ad842e0a0
id <iuliia.davidenko@diamos.com>
2023-12-12 11:22:16 +0100

    test

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -19,14 +19,13 @@ test('parsing', () => {
    // const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
 
     const result = '{\n' +
-        ' - follow: false\n' +
-        '   host: hexlet.io\n' +
-        ' - proxy: 123.234.53.22\n' +
-        ' - timeout: 50\n' +
-        ' + timeout: 20\n' +
-        ' + verbose: true\n' +
+        '  - follow: false\n' +
+        '    host: hexlet.io\n' +
+        '  - proxy: 123.234.53.22\n' +
+        '  - timeout: 50\n' +
+        '  + timeout: 20\n' +
+        '  + verbose: true\n' +
         '}';
-
 
     expect(genDiff('__tests__/__fixtures__/json/file1.json', '__tests__/__fixtures__/json/file2.json', 'stylish')).toStrictEqual(result);
 });
@@ -41,12 +40,12 @@ test('parsing2', () => {
 
     //const result = {"  host": "hexlet.io", "+ timeout": 20, "+ verbose": true, "- follow": false, "- proxy": "123.234.53.22", "- timeout": 50}
     const result = '{\n' +
-        ' - follow: false\n' +
-        '   host: hexlet.io\n' +
-        ' - proxy: 123.234.53.22\n' +
-        ' - timeout: 50\n' +
-        ' + timeout: 20\n' +
-        ' + verbose: true\n' +
+        '  - follow: false\n' +
+        '    host: hexlet.io\n' +
+        '  - proxy: 123.234.53.22\n' +
+        '  - timeout: 50\n' +
+        '  + timeout: 20\n' +
+        '  + verbose: true\n' +
         '}'
 
 
@@ -111,7 +110,7 @@ test('parsing', () => {
         '     }\n' +
         '}';*/
 
-    const result = '{\n' +
+    /*const result = '{\n' +
         '   common: {\n' +
         '    + follow: false\n' +
         '      setting1: Value 1\n' +
@@ -154,7 +153,52 @@ test('parsing', () => {
         '      }\n' +
         '      fee: 100500\n' +
         '     }\n' +
-        '}';
+        '}';*/
+
+    const result = '{\n' +
+    '    common: {\n' +
+    '      + follow: false\n' +
+    '        setting1: Value 1\n' +
+    '      - setting2: 200\n' +
+    '      - setting3: true\n' +
+    '      + setting3: null\n' +
+    '      + setting4: blah blah\n' +
+    '      + setting5: {\n' +
+    '            key5: value5\n' +
+    '        }\n' +
+    '        setting6: {\n' +
+    '            doge: {\n' +
+    '              - wow: \n' +
+    '              + wow: so much\n' +
+    '            }\n' +
+    '            key: value\n' +
+    '          + ops: vops\n' +
+    '        }\n' +
+    '    }\n' +
+    '    group1: {\n' +
+    '      - baz: bas\n' +
+    '      + baz: bars\n' +
+    '        foo: bar\n' +
+    '      - nest: {\n' +
+    '            key: value\n' +
+    '        }\n' +
+    '      + nest: str\n' +
+    '    }\n' +
+    '  - group2: {\n' +
+    '        abc: 12345\n' +
+    '        deep: {\n' +
+    '            id: 45\n' +
+    '        }\n' +
+    '    }\n' +
+    '  + group3: {\n' +
+    '        deep: {\n' +
+    '            id: {\n' +
+    '                number: 45\n' +
+    '            }\n' +
+    '        }\n' +
+    '        fee: 100500\n' +
+    '    }\n' +
+    '}'
 
 
     expect(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json')).toStrictEqual(result);
