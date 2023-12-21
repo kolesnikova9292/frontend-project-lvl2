26eff5bafdda6c789ebb8d071866e1d3157cad0a
id <iuliia.davidenko@diamos.com>
2023-12-21 17:39:27 +0100

    lint

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,7 @@
-const addStylishFormater = (stylish = { replacer: ' ', spacesCount: 1, step: 1 }, variable = '', valueOfVariable = '', addedOrRemovedOrTheSame = '') =>
-  `${stylish.replacer.repeat((stylish.step - 1) * (stylish.spacesCount + 2))}`
-   + `${stylish.replacer.repeat(stylish.step * stylish.spacesCount)} ${addedOrRemovedOrTheSame} `
-   + `${variable ?? ''}${variable ? ': ' : ''}${valueOfVariable ?? ''}${variable ? '\n' : ''}`;
+const addStylishFormater = (stylish = { replacer: ' ', spacesCount: 1, step: 1 },
+  variable = '',
+  valueOfVariable = '',
+  addedOrRemovedOrTheSame = '') => `${stylish.replacer.repeat((stylish.step - 1) * (stylish.spacesCount + 2))}`
+  + `${stylish.replacer.repeat(stylish.step * stylish.spacesCount)} ${addedOrRemovedOrTheSame} `
+  + `${variable ?? ''}${variable ? ': ' : ''}${valueOfVariable ?? ''}${variable ? '\n' : ''}`;
 export default addStylishFormater;
