83c11edad6146b69d5c3f94abe26ca3cdd773da0
id <iuliia.davidenko@diamos.com>
2023-12-21 16:12:26 +0100

    lint

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -101,65 +101,20 @@ test('parsing5', () => {
   expect(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json', 'plain')).toStrictEqual(result);
 });
 
-test('parsing', () => {
-    const result = {
-        "-follow": false,
-        " host": "hexlet.io",
-        "-proxy": "123.234.53.22",
-        "-timeout": 50,
-        "+timeout": 20,
-        "+verbose": true,
-    };
-    expect(genDiff('__tests__/__fixtures__/json/file1.json', '__tests__/__fixtures__/json/file2.json', 'json')).toStrictEqual(result);
-    expect(genDiff('__tests__/__fixtures__/json/file1.json', '__tests__/__fixtures__/json/file2.json', 'json')["-follow"]).toStrictEqual(false);
+test('parsing6', () => {
+  const result = {
+    "-follow": false,
+    " host": "hexlet.io",
+    "-proxy": "123.234.53.22",
+    "-timeout": 50,
+    "+timeout": 20,
+    "+verbose": true,
+  };
+  expect(genDiff('__tests__/__fixtures__/json/file1.json', '__tests__/__fixtures__/json/file2.json', 'json')).toStrictEqual(result);
+  expect(genDiff('__tests__/__fixtures__/json/file1.json', '__tests__/__fixtures__/json/file2.json', 'json')["-follow"]).toStrictEqual(false);
 });
 
-test('parsing', () => {
-    const result = {
-        " common": {
-            "+follow": false,
-    " setting1": "Value 1",
-    "-setting2": 200,
-    "-setting3": true,
-    "+setting3": "null",
-    "+setting4": "blah blah",
-    "+setting5": {
-        " key5": "value5",
-    },
-    " setting6": {
-        " doge": {
-            "-wow": "",
-            "+wow": "so much",
-        },
-        " key": "value",
-        "+ops": "vops",
-    },
-},
-    " group1": {
-        "-baz": "bas",
-        "+baz": "bars",
-        " foo": "bar",
-        "-nest": {
-            " key": "value",
-        },
-        "+nest": "str",
-    },
-    "-group2": {
-        " abc": 12345,
-        " deep": {
-            " id": 45,
-        }
-    },
-    "+group3": {
-        " deep": {
-            " id": {
-                " number": 45
-            },
-        },
-        " fee": 100500
-    }
-    };
-
-    expect(JSON.parse(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json', 'json'))[" common"]["+follow"]).toEqual("false");
-    expect(JSON.parse(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json', 'json'))["+group3"][" fee"]).toEqual("100500");
+test('parsing7', () => {
+  expect(JSON.parse(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json', 'json'))[" common"]["+follow"]).toEqual("false");
+  expect(JSON.parse(genDiff('__tests__/__fixtures__/json2/file1.json', '__tests__/__fixtures__/json2/file2.json', 'json'))["+group3"][" fee"]).toEqual("100500");
 });
