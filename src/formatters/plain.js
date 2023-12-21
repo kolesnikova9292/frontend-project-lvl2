0aa0f17e3a42ad244b5b83f03a252adb8fbca38c
id <iuliia.davidenko@diamos.com>
2023-12-21 17:29:56 +0100

    lint

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,29 +1,28 @@
 const addPlainFormatter = (variable, valueOfVariable, addedOrRemovedOrTheSame, updated) => {
   if (updated === 'old') {
-    return `Property '${variable ?? ''}' was updated. From ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}`;
+    return `Property '${variable ?? ''}' was updated. From ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]' && valueOfVariable !== 'null' ? `'${valueOfVariable}'` : valueOfVariable}`;
   }
 
   if (updated === 'new') {
-    return ` to ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]' && valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}\n`;
+    return ` to ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]' && valueOfVariable !== 'null' ? `'${valueOfVariable}'` : valueOfVariable}\n`;
   }
 
   if (addedOrRemovedOrTheSame === '-') {
-    return `Property \'${variable ?? ''}\' was removed\n`;
+    return `Property '${variable ?? ''}' was removed\n`;
   }
 
   if (addedOrRemovedOrTheSame === '+') {
-    return `Property '${variable ?? ''}' was added with value: ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}\n`;
+    return `Property '${variable ?? ''}' was added with value: ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null' ? `'${valueOfVariable}'` : valueOfVariable}\n`;
   }
 
   if (addedOrRemovedOrTheSame === ' ') {
     if (valueOfVariable.indexOf('Property') > -1) {
-      const regex = /Property \'/g;
-      valueOfVariable = valueOfVariable.replace(regex, `Property \'${variable}.`);
-      return valueOfVariable;
+      const regex = /Property '/g;
+      return valueOfVariable.replace(regex, `Property \'${variable}.`);
     }
     return '';
   }
 
-  return `Property \'${variable ?? ''}' was added with value: ${valueOfVariable ?? ''}\n`;
+  return `Property '${variable ?? ''}' was added with value: ${valueOfVariable ?? ''}\n`;
 };
 export default addPlainFormatter;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
-const addStylishFormater = (stylish = { replacer: ' ', spacesCount: 1, step: 1 }, variable, valueOfVariable, addedOrRemovedOrTheSame) =>{
-    //console.log(stylish.step + ' ' + variable)
-    //return `${stylish.replacer.repeat(stylish.step * stylish.spacesCount)} ${addedOrRemovedOrTheSame} ${variable ?? ''}${variable ? ': ' : ''}${valueOfVariable ?? ''} step: ${stylish.step} ${variable ? '\n' : ''}`;
-    return `${stylish.replacer.repeat((stylish.step-1) * (stylish.spacesCount + 2))}${stylish.replacer.repeat(stylish.step * stylish.spacesCount)} ${addedOrRemovedOrTheSame} ${variable ?? ''}${variable ? ': ' : ''}${valueOfVariable ?? ''}${variable ? '\n' : ''}`;
-}
+const addStylishFormater = (stylish = { replacer: ' ', spacesCount: 1, step: 1 }, variable = '', valueOfVariable = '', addedOrRemovedOrTheSame = '') => {
+  return `${stylish.replacer.repeat((stylish.step-1) * (stylish.spacesCount + 2))}`
+    + `${stylish.replacer.repeat(stylish.step * stylish.spacesCount)} ${addedOrRemovedOrTheSame} `
+    + `${variable ?? ''}${variable ? ': ' : ''}${valueOfVariable ?? ''}${variable ? '\n' : ''}`;
+};
 export default addStylishFormater;
