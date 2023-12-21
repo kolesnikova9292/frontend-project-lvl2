2738bcbf8bb80b9c36c6fd80e3de41030d35dcb4
id <iuliia.davidenko@diamos.com>
2023-12-21 17:23:33 +0100

    lint

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,9 +1,8 @@
 const addJsonFormater = (variable, valueOfVariable, addedOrRemovedOrTheSame) => {
-  if (valueOfVariable.toString().substring(0,1) === '{') {
+  if (valueOfVariable.toString().substring(0, 1) === '{') {
     return { variable: addedOrRemovedOrTheSame + variable, value: valueOfVariable };
-  } else {
-    return { variable: addedOrRemovedOrTheSame + variable, value: '"' + valueOfVariable + '"' };
   }
+  return { variable: addedOrRemovedOrTheSame + variable, value: `"${valueOfVariable}"` };
 };
 
 export default addJsonFormater;
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,23 +1,22 @@
 const addPlainFormatter = (variable, valueOfVariable, addedOrRemovedOrTheSame, updated) => {
-  if(updated === 'old') {
-    return `Property \'${variable ?? ''}\' was updated. From ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}`;
+  if (updated === 'old') {
+    return `Property '${variable ?? ''}' was updated. From ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}`;
   }
 
-  if(updated === 'new') {
+  if (updated === 'new') {
     return ` to ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]' && valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}\n`;
   }
 
-  if(addedOrRemovedOrTheSame === '-') {
+  if (addedOrRemovedOrTheSame === '-') {
     return `Property \'${variable ?? ''}\' was removed\n`;
   }
 
-  if(addedOrRemovedOrTheSame === '+') {
-    return `Property \'${variable ?? ''}\' was added with value: ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}\n`;
+  if (addedOrRemovedOrTheSame === '+') {
+    return `Property '${variable ?? ''}' was added with value: ${typeof valueOfVariable === 'string' && valueOfVariable !== '[complex value]'&& valueOfVariable !== 'null'  ? `'${valueOfVariable}'` : valueOfVariable}\n`;
   }
 
-  if(addedOrRemovedOrTheSame === ' ') {
-    if(valueOfVariable.indexOf('Property') > -1) {
-      const indexOfQuote = valueOfVariable.indexOf('\'');
+  if (addedOrRemovedOrTheSame === ' ') {
+    if (valueOfVariable.indexOf('Property') > -1) {
       const regex = /Property \'/g;
       valueOfVariable = valueOfVariable.replace(regex, `Property \'${variable}.`);
       return valueOfVariable;
