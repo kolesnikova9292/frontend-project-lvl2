83f76dad7e6c3c8ba84ccabb4c9053e3266ec678
id <iuliia.davidenko@diamos.com>
2023-12-21 16:43:43 +0100

    lint

--- genDiff.js
+++ genDiff.js
@@ -6,20 +6,6 @@ import * as yaml from 'js-yaml';
 import { startResult } from './src/formatters/index.js';
 import parsing from './src/parsing.js';
 
-program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish')
-  .arguments('<filepath1>')
-  .arguments('<filepath2>')
-  .allowUnknownOption(true)
-  .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2, program.opts().format);
-    console.log(result);
-  });
-
-program.parse();
-
 export default function genDiff(fileName1, fileName2, formatter = 'stylish', replacer = ' ', spacesCount = 1, result = startResult(formatter), step = 1) {
   const extension1 = path.extname(fileName1);
   const extension2 = path.extname(fileName2);
@@ -42,23 +28,34 @@ export default function genDiff(fileName1, fileName2, formatter = 'stylish', rep
     return resultObject;
   }
 
+  if ((extension1 === '.yaml' && extension2 === '.yaml') || (extension1 === '.yml' && extension2 === '.yml')) {
 
-    if ((extension1 === '.yaml' && extension2 === '.yaml') || (extension1 === '.yml' && extension2 === '.yml')) {
+    const data = fs.readFileSync(file1, 'utf8');
+    const data1 = fs.readFileSync(file2, 'utf8');
 
-        const data = fs.readFileSync(file1, 'utf8');
-        const data1 = fs.readFileSync(file2, 'utf8');
+    const doc = yaml.load(data);
+    const doc2 = yaml.load(data1);
 
-        const doc = yaml.load(data);
-        const doc2 = yaml.load(data1);
+    let resultObject = parsing(doc, doc2, formatter, replacer, spacesCount, result, step);
 
-        let resultObject = parsing(doc, doc2, formatter, replacer, spacesCount, result, step);
-
-        if(formatter === 'plain') {
-            resultObject = resultObject.slice(0, -1);
-        }
-
-        return resultObject;
+    if(formatter === 'plain') {
+      resultObject = resultObject.slice(0, -1);
     }
 
-    return;
+    return resultObject;
+  }
 }
+
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
+    .allowUnknownOption(true)
+    .action((filepath1, filepath2) => {
+        const result = genDiff(filepath1, filepath2, program.opts().format);
+        console.log(result);
+    });
+
+program.parse();
