ff07f406d657d3e99502f4a89aea206b937af632
id <iuliia.davidenko@diamos.com>
2023-12-21 16:56:24 +0100

    lint

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,67 +2,63 @@ import addStylishFormater from './stylish.js';
 import addPlainFormatter from './plain.js';
 import addJsonFormater from './json.js';
 
-const chainResult = (formatter = 'stylish',
-                     result,
-                     nextChain) => {
-  if(formatter === 'stylish') {
+const chainResult = (formatter = 'stylish', result, nextChain) => {
+  if (formatter === 'stylish') {
     return result + nextChain;
   }
 
-  if(formatter === 'plain') {
+  if (formatter === 'plain') {
     return result + nextChain;
   }
 
-  if(formatter === 'json') {
+  if (formatter === 'json') {
     return result + '"' + nextChain.variable + '"' + ': ' + nextChain.value + ', ';
   }
 }
 
 const addFormating = (formatter = 'stylish',
-                      stylish = { replacer: ' ', spacesCount: 1, step: 1 },
-                      variable,
-                      valueOfVariable,
-                      addedOrRemovedOrTheSame = ' ',
-                      updated = '') => {
-  if(formatter === 'stylish') {
+  stylish = { replacer: ' ', spacesCount: 1, step: 1 },
+  variable,
+  valueOfVariable,
+  addedOrRemovedOrTheSame = ' ',
+  updated = '') => {
+  if (formatter === 'stylish') {
     return addStylishFormater(stylish, variable, valueOfVariable, addedOrRemovedOrTheSame);
   }
 
-  if(formatter === 'plain') {
+  if (formatter === 'plain') {
     return addPlainFormatter(variable, valueOfVariable, addedOrRemovedOrTheSame, updated);
   }
 
-  if(formatter === 'json') {
+  if (formatter === 'json') {
     return addJsonFormater(variable, valueOfVariable, addedOrRemovedOrTheSame);
   }
 }
 
 const startResult = (formatter = 'stylish') => {
-  if(formatter === 'stylish') {
+  if (formatter === 'stylish') {
     return '{\n';
   }
 
-  if(formatter === 'plain') {
+  if (formatter === 'plain') {
     return '';
   }
 
-  if(formatter === 'json') {
+  if (formatter === 'json') {
     return '{'
   }
 }
 
-const endResult = (formatter = 'stylish',
-                   result,
-                   stylish = { replacer: ' ', spacesCount: 1, step: 1 }) => {
-  if(formatter === 'stylish') {
+const endResult = (formatter = 'stylish', result, stylish = { replacer: ' ', spacesCount: 1, step: 1 }) => {
+  if (formatter === 'stylish') {
     return result + stylish.replacer.repeat(4 * stylish.step - 4) + '}';
   }
 
-  if(formatter === 'plain') {
+  if (formatter === 'plain') {
     return result;
   }
 
-  if(formatter === 'json') {
+  if (formatter === 'json') {
     return result.slice(0, -2) + ' }';
   }
 }
