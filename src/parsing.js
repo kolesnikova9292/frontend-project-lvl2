004e8180771cb3b08202375cb9d9fd921c898d7b
id <iuliia.davidenko@diamos.com>
2023-12-29 16:41:02 +0100

    lint

--- src/parsing.js
+++ src/parsing.js
@@ -52,7 +52,9 @@ const parsing = (json1, json2, formatter = 'stylish', replacer = ' ', spacesCoun
     if (stringifyLittle(json1[x.key]) === stringifyLittle(json2[x.key])
       && arrayWithInsertedProps.indexOf(x.key) === -1) {
       arrayWithInsertedProps.push(x.key);
-      return chainResult(formatter, accumulator,
+      return chainResult(
+        formatter,
+        accumulator,
         addFormating(formatter, { replacer, spacesCount, step }, x.key,
           stringifyLittle(json1[x.key], formatter, replacer, spacesCount, step)));
     }
