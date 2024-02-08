fe8793d08c280477c3dbeef80ee8efaf7c0a7c72
id <iuliia.davidenko@diamos.com>
2024-02-08 09:55:34 +0100

    linter

--- src/genDiffMain.js
+++ src/genDiffMain.js
@@ -5,7 +5,7 @@ import * as yaml from 'js-yaml';
 import { startResult } from './formatters/index.js';
 import parsing from './parsing.js';
 
-const getDataByType = (fileName) => {
+const getDataType = (fileName) => {
   const dateType = path.extname(fileName).slice(1);
   const filePath = path.resolve(fileName);
   const data = fs.readFileSync(filePath, 'utf8');
@@ -17,12 +17,11 @@ const getDataByType = (fileName) => {
     return yaml.load(data);
   }
   return null;
-}
+};
 
 export default function genDiffMain(fileName1, fileName2, formatter = 'stylish') {
-  const resultObject = parsing(
-      getDataByType(fileName1), getDataByType(fileName2), formatter, startResult(formatter)
-  );
+  const resultObject =
+      parsing(getDataType(fileName1), getDataType(fileName2), formatter, startResult(formatter));
   if (formatter === 'plain') {
     return resultObject.slice(0, -1);
   }
