f75ef513c9e065be60d1447875775b8e497010db
id <iuliia.davidenko@diamos.com>
2024-02-08 09:47:52 +0100

    function

--- src/genDiffMain.js
+++ src/genDiffMain.js
@@ -6,13 +6,13 @@ import { startResult } from './formatters/index.js';
 import parsing from './parsing.js';
 
 export default function genDiffMain(fileName1, fileName2, formatter = 'stylish') {
-  const extension1 = path.extname(fileName1);
-  const extension2 = path.extname(fileName2);
+  //const extension1 = path.extname(fileName1);
+ // const extension2 = path.extname(fileName2);
 
-  const file1 = path.resolve(fileName1);
-  const file2 = path.resolve(fileName2);
+  //const file1 = path.resolve(fileName1);
+  //const file2 = path.resolve(fileName2);
 
-  if (extension1 === '.json' && extension2 === '.json') {
+  /*if (extension1 === '.json' && extension2 === '.json') {
     const data = fs.readFileSync(file1, 'utf8');
     const data1 = fs.readFileSync(file2, 'utf8');
     const json1 = JSON.parse(data);
@@ -25,9 +25,9 @@ export default function genDiffMain(fileName1, fileName2, formatter = 'stylish')
     }
 
     return resultObject;
-  }
+  }*/
 
-  if ((extension1 === '.yaml' && extension2 === '.yaml') || (extension1 === '.yml' && extension2 === '.yml')) {
+  /*if ((extension1 === '.yaml' && extension2 === '.yaml') || (extension1 === '.yml' && extension2 === '.yml')) {
     const data = fs.readFileSync(file1, 'utf8');
     const data1 = fs.readFileSync(file2, 'utf8');
 
@@ -41,6 +41,27 @@ export default function genDiffMain(fileName1, fileName2, formatter = 'stylish')
     }
 
     return resultObject;
+  }*/
+
+  const resultObject = parsing(getDataByType(fileName1), getDataByType(fileName2), formatter, startResult(formatter));
+  if (formatter === 'plain') {
+    return resultObject.slice(0, -1);
   }
-  return null;
+
+  return resultObject;
+
+  //return null;
+}
+
+const getDataByType = (fileName) => {
+  const dateType = path.extname(fileName).slice(1);
+  const filePath = path.resolve(fileName);
+  const data = fs.readFileSync(filePath, 'utf8');
+
+  if(dateType === 'json') {
+    return JSON.parse(data);
+  }
+  if(dateType === 'yaml' || dateType === 'yml') {
+    return yaml.load(data);
+  }
 }
