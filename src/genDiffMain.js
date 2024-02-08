edc108dd5e8e69922b04c6e04c06c0875c9e16ed
id <iuliia.davidenko@diamos.com>
2024-02-08 09:52:53 +0100

    lint

--- src/genDiffMain.js
+++ src/genDiffMain.js
@@ -5,24 +5,27 @@ import * as yaml from 'js-yaml';
 import { startResult } from './formatters/index.js';
 import parsing from './parsing.js';
 
-export default function genDiffMain(fileName1, fileName2, formatter = 'stylish') {
-  const resultObject = parsing(getDataByType(fileName1), getDataByType(fileName2), formatter, startResult(formatter));
-  if (formatter === 'plain') {
-    return resultObject.slice(0, -1);
-  }
-
-  return resultObject;
-}
-
 const getDataByType = (fileName) => {
   const dateType = path.extname(fileName).slice(1);
   const filePath = path.resolve(fileName);
   const data = fs.readFileSync(filePath, 'utf8');
 
-  if(dateType === 'json') {
+  if (dateType === 'json') {
     return JSON.parse(data);
   }
-  if(dateType === 'yaml' || dateType === 'yml') {
+  if (dateType === 'yaml' || dateType === 'yml') {
     return yaml.load(data);
   }
+  return null;
+}
+
+export default function genDiffMain(fileName1, fileName2, formatter = 'stylish') {
+  const resultObject = parsing(
+      getDataByType(fileName1), getDataByType(fileName2), formatter, startResult(formatter)
+  );
+  if (formatter === 'plain') {
+    return resultObject.slice(0, -1);
+  }
+
+  return resultObject;
 }
