823fab930a6c24718180e7214071948ebb9e7545
id <iuliia.davidenko@diamos.com>
2024-03-11 13:34:10 +0100

    lint

--- src/buildTree.js
+++ src/buildTree.js
@@ -7,89 +7,89 @@ const stringify = (value) => {
       return `${currentValue}`;
     }
     const lines = Object
-        .entries(currentValue)
-        .map(([key, val]) =>{
+      .entries(currentValue)
+      .map(([key, val]) =>{
 
-          const nodeValue = iter(val, depth + 1);
-          const resultValue = { key };
+        const nodeValue = iter(val, depth + 1);
+        const resultValue = { key };
 
-          if (!_.isObject(nodeValue)) {
-            return { ...resultValue, value: nodeValue }
-          }
+        if (!_.isObject(nodeValue)) {
+          return { ...resultValue, value: nodeValue }
+        }
 
-          return { key: key, children: nodeValue }
-          }
-        );
-    return [ ...lines ]
+        return { key: key, children: nodeValue }
+        }
+      );
+    return [ ...lines ];
   };
 
   return iter(value, 1);
 };
 
 export const hasObjectThisProp = (object, prop) => {
-    if(object.map(x => x.key).indexOf(prop) <= -1) {
-        return false;
-    }
-    if(object.map(x => x.key).indexOf(prop) > -1) {
-        return true;
-    }
+  if(object.map(x => x.key).indexOf(prop) <= -1) {
+    return false;
+  }
+  if(object.map(x => x.key).indexOf(prop) > -1) {
+    return true;
+  }
 }
 
 const getElementByKey = (object, prop) => {
-    const index = object.map(x => x.key).indexOf(prop);
-    return object[index];
+  const index = object.map(x => x.key).indexOf(prop);
+  return object[index];
 }
 
 const commonTree = (nodeArray1, nodeArray2) => {
 
-    const iter = (nodeArray1, nodeArray2, depth) => {
-        if(nodeArray1.length === 0 && nodeArray2.length === 0) {
-            return [];
+  const iter = (nodeArray1, nodeArray2, depth) => {
+    if(nodeArray1.length === 0 && nodeArray2.length === 0) {
+      return [];
+    }
+    const keys = _.union(nodeArray1.map(x => x.key), nodeArray2.map(x => x.key));
+    const lines1 = keys.reduce((accumulator, currentValue) => {
+      if(hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)
+        && _.isEqual(getElementByKey(nodeArray1, currentValue), getElementByKey(nodeArray2, currentValue))) {
+        return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'unchanged' } ];
+      }
+      if(hasObjectThisProp(nodeArray1, currentValue) && !hasObjectThisProp(nodeArray2, currentValue)) {
+        return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'deleted' } ]
+      }
+      if(!hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)) {
+        return [ ...accumulator, { ...getElementByKey(nodeArray2, currentValue), type: 'added' } ]
+      }
+
+      if(hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)
+        && !_.isEqual(getElementByKey(nodeArray1, currentValue), getElementByKey(nodeArray2, currentValue))) {
+        if(!_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['value'])) {
+          return [ ...accumulator, { ...getElementByKey(nodeArray2, currentValue), oldValue: getElementByKey(nodeArray1, currentValue)['value'], type: 'changed' } ];
         }
-        const keys = _.union(nodeArray1.map(x => x.key), nodeArray2.map(x => x.key));
-        const lines1 = keys.reduce((accumulator, currentValue) => {
-            if(hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)
-                && _.isEqual(getElementByKey(nodeArray1, currentValue), getElementByKey(nodeArray2, currentValue))) {
-                return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'unchanged' } ]
-            }
-            if(hasObjectThisProp(nodeArray1, currentValue) && !hasObjectThisProp(nodeArray2, currentValue)) {
-                return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'deleted' } ]
-            }
-            if(!hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)) {
-                return [ ...accumulator, { ...getElementByKey(nodeArray2, currentValue), type: 'added' } ]
-            }
 
-            if(hasObjectThisProp(nodeArray1, currentValue) && hasObjectThisProp(nodeArray2, currentValue)
-                && !_.isEqual(getElementByKey(nodeArray1, currentValue), getElementByKey(nodeArray2, currentValue))) {
-                if(!_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['value'])) {
-                    return [ ...accumulator, { ...getElementByKey(nodeArray2, currentValue), oldValue: getElementByKey(nodeArray1, currentValue)['value'], type: 'changed' } ];
-                }
+        if(_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray1, currentValue)['children']) &&
+          _.isNil(getElementByKey(nodeArray2, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['children'])) {
+          return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue),
+            children: _.orderBy(iter(getElementByKey(nodeArray1, currentValue)['children'], getElementByKey(nodeArray2, currentValue)['children'], depth + 1),
+                ['key'], ['asc']), type: 'nested' } ];
+        }
 
-                if(_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray1, currentValue)['children']) &&
-                    _.isNil(getElementByKey(nodeArray2, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['children'])) {
-                    return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue),
-                        children: _.orderBy(iter(getElementByKey(nodeArray1, currentValue)['children'], getElementByKey(nodeArray2, currentValue)['children'], depth + 1),
-                            ['key'], ['asc']), type: 'nested' } ];
-                }
+        if(_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['value']) &&
+          !_.isNil(getElementByKey(nodeArray1, currentValue)['children']) && _.isNil(getElementByKey(nodeArray2, currentValue)['children'])) {
 
-                if(_.isNil(getElementByKey(nodeArray1, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray2, currentValue)['value']) &&
-                    !_.isNil(getElementByKey(nodeArray1, currentValue)['children']) && _.isNil(getElementByKey(nodeArray2, currentValue)['children'])) {
-
-                    return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue),
-                        children: _.orderBy(iter(getElementByKey(nodeArray1, currentValue)['children'], getElementByKey(nodeArray1, currentValue)['children'], depth + 1),
-                            ['key'], ['asc']), type: 'deleted' }, { ...getElementByKey(nodeArray2, currentValue), type: 'added' } ]
+            return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue),
+              children: _.orderBy(iter(getElementByKey(nodeArray1, currentValue)['children'], getElementByKey(nodeArray1, currentValue)['children'], depth + 1),
+                  ['key'], ['asc']), type: 'deleted' }, { ...getElementByKey(nodeArray2, currentValue), type: 'added' } ]
 
                 }
 
-                if(_.isNil(getElementByKey(nodeArray2, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray1, currentValue)['value']) &&
-                    !_.isNil(getElementByKey(nodeArray2, currentValue)['children']) && _.isNil(getElementByKey(nodeArray1, currentValue)['children'])) {
+        if(_.isNil(getElementByKey(nodeArray2, currentValue)['value']) && !_.isNil(getElementByKey(nodeArray1, currentValue)['value']) &&
+          !_.isNil(getElementByKey(nodeArray2, currentValue)['children']) && _.isNil(getElementByKey(nodeArray1, currentValue)['children'])) {
 
-                    return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'deleted' }, { ...getElementByKey(nodeArray2, currentValue),
-                        children: _.orderBy(iter(getElementByKey(nodeArray2, currentValue)['children'], getElementByKey(nodeArray2, currentValue)['children'], depth + 1),
-                            ['key'], ['asc']), type: 'added' } ]
-                }
+          return [ ...accumulator, { ...getElementByKey(nodeArray1, currentValue), type: 'deleted' }, { ...getElementByKey(nodeArray2, currentValue),
+            children: _.orderBy(iter(getElementByKey(nodeArray2, currentValue)['children'], getElementByKey(nodeArray2, currentValue)['children'], depth + 1),
+            ['key'], ['asc']), type: 'added' } ]
             }
-            return [ ...accumulator ];
+          }
+          return [ ...accumulator ];
         }, []);
         return lines1;
     };
@@ -99,12 +99,12 @@ const commonTree = (nodeArray1, nodeArray2) => {
 
 const buildTree = (json1, json2) => {
 
-    const nodeArray1 = stringify(json1);
-    const nodeArray2 = stringify(json2);
+  const nodeArray1 = stringify(json1);
+  const nodeArray2 = stringify(json2);
 
-    const commonTreeResult = commonTree(nodeArray1, nodeArray2);
+  const commonTreeResult = commonTree(nodeArray1, nodeArray2);
 
-    return _.orderBy(commonTreeResult, ['key'], ['asc']);
+  return _.orderBy(commonTreeResult, ['key'], ['asc']);
 };
 
 export default buildTree;
