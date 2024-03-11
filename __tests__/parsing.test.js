4037eb30eae150eae705e2e02a87cc89d2791105
id <iuliia.davidenko@diamos.com>
2024-03-11 12:46:31 +0100

    lint

--- __tests__/parsing.test.js
+++ __tests__/parsing.test.js
@@ -4,17 +4,17 @@ import { resultStylish, resultPlain } from '../__fixtures__/json/result.js'
 import { resultStylish as resultStylish1, resultPlain as resultPlain1 } from '../__fixtures__/json2/result.js'
 import result3 from '../__fixtures__/yaml/result.js'
 
-  test.each([
-    ['__fixtures__/json/file1.json', '__fixtures__/json/file2.json', 'stylish', resultStylish],
-    ['__fixtures__/json2/file1.json', '__fixtures__/json2/file2.json', 'stylish', resultStylish1],
-    ['__fixtures__/yaml/file1.yaml', '__fixtures__/yaml/file2.yaml', 'stylish', result3],
-    ['__fixtures__/json/file1.json', '__fixtures__/json/file2.json', 'plain', resultPlain],
-    ['__fixtures__/json2/file1.json', '__fixtures__/json2/file2.json', 'plain', resultPlain1]
-  ])('genDIffMain', (file1, file2, formatter, result) => {
-    expect(genDiff(file1, file2, formatter)).toStrictEqual(
-        result,
-    );
-  });
+test.each([
+  ['__fixtures__/json/file1.json', '__fixtures__/json/file2.json', 'stylish', resultStylish],
+  ['__fixtures__/json2/file1.json', '__fixtures__/json2/file2.json', 'stylish', resultStylish1],
+  ['__fixtures__/yaml/file1.yaml', '__fixtures__/yaml/file2.yaml', 'stylish', result3],
+  ['__fixtures__/json/file1.json', '__fixtures__/json/file2.json', 'plain', resultPlain],
+  ['__fixtures__/json2/file1.json', '__fixtures__/json2/file2.json', 'plain', resultPlain1]
+])('genDIffMain', (file1, file2, formatter, result) => {
+  expect(genDiff(file1, file2, formatter)).toStrictEqual(
+      result,
+  );
+});
 
 test('parsing6', () => {
   const rez = genDiff('__fixtures__/json/file1.json', '__fixtures__/json/file2.json', 'json');
