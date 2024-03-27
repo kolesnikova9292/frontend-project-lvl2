2c0a5e6a00277591a9d7ebd47c1521b63a8c6586
id <iuliia.davidenko@diamos.com>
2024-03-27 15:46:22 +0100

    komments

--- src/buildTree.js
+++ src/buildTree.js
@@ -26,12 +26,10 @@ const stringify = (value) => {
 
 const commonTree = (nodeArrayFirst, nodeArraySecond) => {
   const iter = (nodeArray1, nodeArray2, depth) => {
-    console.log(nodeArray1)
     if (nodeArray1.length === 0 && nodeArray2.length === 0) {
       return [];
     }
     const keys = _.union(nodeArray1.map((x) => x.key), nodeArray2.map((x) => x.key));
-    console.log(keys)
     const lines1 = keys.flatMap((currentValue) => {
       const objFromFirst = _.find(nodeArray1, (item) => item.key === currentValue);
       const objFromSecond = _.find(nodeArray2, (item) => item.key === currentValue);
@@ -93,106 +91,7 @@ const commonTree = (nodeArrayFirst, nodeArraySecond) => {
             }];
         }
       }
-      return {};
     });
-    /*const lines1 = keys.reduce((accumulator, currentValue) => {
-      if (_.some(nodeArray1, (item) => item.key === currentValue)
-        && _.some(nodeArray2, (item) => item.key === currentValue)
-        && _.isEqual(
-          _.find(nodeArray1, (item) => item.key === currentValue),
-          _.find(nodeArray2, (item) => item.key === currentValue),
-        )
-      ) {
-        return [...accumulator, { ..._.find(nodeArray1, (item) => item.key === currentValue), type: 'unchanged' }];
-      }
-      if (_.some(nodeArray1, (item) => item.key === currentValue)
-        && !_.some(nodeArray2, (item) => item.key === currentValue)) {
-        return [...accumulator, { ..._.find(nodeArray1, (item) => item.key === currentValue), type: 'deleted' }];
-      }
-      if (!_.some(nodeArray1, (item) => item.key === currentValue)
-        && _.some(nodeArray2, (item) => item.key === currentValue)) {
-        return [...accumulator, { ..._.find(nodeArray2, (item) => item.key === currentValue), type: 'added' }];
-      }
-
-      if (_.some(nodeArray1, (item) => item.key === currentValue)
-        && _.some(nodeArray2, (item) => item.key === currentValue)
-        && !_.isEqual(
-          _.find(nodeArray1, (item) => item.key === currentValue),
-          _.find(nodeArray2, (item) => item.key === currentValue),
-        )) {
-        if (!_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray2, (item) => item.key === currentValue).value)) {
-          return [...accumulator,
-            {
-              ..._.find(nodeArray2, (item) => item.key === currentValue),
-              oldValue: _.find(nodeArray1, (item) => item.key === currentValue).value,
-              type: 'changed',
-            }];
-        }
-
-        if (_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).children)
-          && _.isNil(_.find(nodeArray2, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray2, (item) => item.key === currentValue).children)) {
-          return [...accumulator, {
-            ..._.find(nodeArray1, (item) => item.key === currentValue),
-            children: _.orderBy(
-              iter(
-                _.find(nodeArray1, (item) => item.key === currentValue).children,
-                _.find(nodeArray2, (item) => item.key === currentValue).children,
-                depth + 1,
-              ),
-              ['key'],
-              ['asc'],
-            ),
-            type: 'nested',
-          }];
-        }
-
-        if (_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray2, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).children)
-          && _.isNil(_.find(nodeArray2, (item) => item.key === currentValue).children)) {
-          return [...accumulator, {
-            ..._.find(nodeArray1, (item) => item.key === currentValue),
-            children: _.orderBy(
-              iter(
-                _.find(nodeArray1, (item) => item.key === currentValue).children,
-                _.find(nodeArray1, (item) => item.key === currentValue).children,
-                depth + 1,
-              ),
-              ['key'],
-              ['asc'],
-            ),
-            type: 'deleted',
-          },
-          { ..._.find(nodeArray2, (item) => item.key === currentValue), type: 'added' },
-          ];
-        }
-
-        if (_.isNil(_.find(nodeArray2, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray1, (item) => item.key === currentValue).value)
-          && !_.isNil(_.find(nodeArray2, (item) => item.key === currentValue).children)
-          && _.isNil(_.find(nodeArray1, (item) => item.key === currentValue).children)) {
-          return [...accumulator,
-            { ..._.find(nodeArray1, (item) => item.key === currentValue), type: 'deleted' },
-            {
-              ..._.find(nodeArray2, (item) => item.key === currentValue),
-              children: _.orderBy(
-                iter(
-                  _.find(nodeArray2, (item) => item.key === currentValue).children,
-                  _.find(nodeArray2, (item) => item.key === currentValue).children,
-                  depth + 1,
-                ),
-                ['key'],
-                ['asc'],
-              ),
-              type: 'added',
-            }];
-        }
-      }
-      return [...accumulator];
-    }, []);*/
     return lines1;
   };
 
