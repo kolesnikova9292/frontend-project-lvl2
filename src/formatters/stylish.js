f58bb77bc6981dccc57fd4ce27dabd391bff7a54
id <iuliia.davidenko@diamos.com>
2024-04-24 10:47:40 +0200

    try

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -61,7 +61,7 @@ const stringify = (tree, replacer = ' ', spacesCount = 1) => {
         return `${currentIndent}${sign(type)}${key}: ${iter(value, depth + 1)}`;
       }
 
-      if (!_.isUndefined(children)) {
+      if (!_.isNil(children)) {
         return `${currentIndent}${sign(type)}${key}: ${iter(children, depth + 1)}`;
       }
       return '';
